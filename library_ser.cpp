c40d114ab4eccc519c37c30bca0c40ae774b810f
ishita-degaonkar <saeedegaonkar10@gmail.com>
2026-09-02 13:07:50 +0530

    Create library_ser.cpp

---
+++ library_ser.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+    int book[5];
+    int searchID;
+    
+    cout<<"Enter 5 Book IDs:\n";
+    
+    for (int i = 0; i < 5; i++)
+ {
+        cin >> book[i];
+ }
+    cout<<"\nEnter Book ID to search";
+    cin>>searchID;
+    for (int i = 0; i<5; i++)
+    {
+        if (book[i]==searchID);
+        {
+            cout<<"Book Found!";
+            return 0;
+        }
+    }
+      cout<<"Book not found!";
+      return 0;
+}
